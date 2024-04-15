bb7b383860117cdcae4c2ee456b8eef3b141ea82
Edisson Arias Cruz <edisson.arias@gmail.com>
2024-04-14 19:51:13 -0700

    demo on how to avoid code duplication

--- java/FizzBash/src/Game.java
+++ java/FizzBash/src/Game.java
@@ -11,19 +11,7 @@ public class Game {
         System.out.println("=================== WELCOME TO FIZZBUZZ ===================");
         System.out.println("-----------------------------------------------------------");
 
-        
-        while(loopGuard) {                                                      // Have Phases instead of while loops (Phases.java)
-            System.out.println("Enter the amount of numbers: ");
-            current = input.getUserInput();
-                if(validator.isNumValid(current)) {
-                    data.setLimit(Integer.parseInt(current));
-                    System.out.println("Limit set to: " + data.getLimit());
-                    loopGuard = false;
-                } else {
-                    loopGuard = true;
-                }
-                
-        }
+        data.setLimit(getInputAsInteger("Enter the amount of numbers", "Limit set to: ", input, validator));
         loopGuard = true;
 
         
@@ -94,6 +82,25 @@ public class Game {
         System.out.println("---------------------------------------------------------");
 
         Algorithm.startAlgorithm(data);
+    }
+
+    public int getInputAsInteger(String ask, String setMessage, inputHandler input, Validator validator)
+    {
+        boolean loopGuard = true;
+        String current;
+        int result = -1;
+        while(loopGuard) {                                                      // Have Phases instead of while loops (Phases.java)
+            System.out.println(ask);
+            current = input.getUserInput();
+                if(validator.isNumValid(current)) {
+                    result = Integer.parseInt(current);
+                    System.out.println(setMessage + " " + result);
+                    loopGuard = false;
+                } else {
+                    loopGuard = true;
+                }
+        }
+        return result;
     }
 
 
