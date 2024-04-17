56b112fe5206f6a7ef17f046903918746cd511ed
TheRealZeros <gabrieldcosta2002@gmail.com>
2024-04-17 16:31:01 +1000

    Fleshed out phases
    Fleshed out how the phases of the game would be handled (CURRENTLY)

--- java/FizzBash/src/Game.java
+++ java/FizzBash/src/Game.java
@@ -2,58 +2,82 @@ public class Game {
 
     Data data = new Data();
 
+    /*
+     * Phases
+     * 
+     * Phase 0
+     *      EXIT
+     * Phase 1
+     *      START
+     * Phase 2
+     *      CONFIG
+     * Phase 3
+     *      DEFAULT
+     * Phase 4
+     *      SETUP
+     * Phase 5
+     *      ALGORITHM
+     * 
+     */
+
     public void startGame() {
 
-       // Phase 1 
+////////// Phase 1 (START)
 
         inputHandler input = new inputHandler();
 
         System.out.println("=================== WELCOME TO FIZZBUZZ ===================");
         System.out.println("-----------------------------------------------------------");
+        System.out.println("Enter 'start' to enter the game or Enter 'exit' to close the game.");
+        /* if start
+         * 
+         * do phase 2 (CONFIG)
+         * 
+         * else if exit
+         * 
+         * do phase 0 (EXIT)
+         *    System.exit(0);
+         * 
+         */
+
+////////// Phase 2 (CONFIG)
 
         System.out.println("You can either start with Default values");
         System.out.println("Or set your own values for the game");
 
-        System.out.println("Enter 'start' to begin the game with the default values");
-        /* if start
+        System.out.println("Enter 'default' to begin the game with the default values");
+        /* if default
          *
          * ask for limit
          * 
-         * do phase 2
+         * do phase 3 (DEFAULT)
          *      data.setBuzz(3);
          *      data.setFizz(5);
          *      data.setFizzName("Fizz");
          *      data.setBuzzName("Buzz");
          * 
-         * do phase 4
+         * do phase 5
          *      Algorithm.startAlgorithm(data);
          */
         System.out.println("Enter 'setup' to set your own values for the game");
         /* if setup
          * 
-         * do phase 3
+         * do phase 4 (SETUP)
          *      ask for limit
          *      ask for fizz
          *      ask for buzz
          *      ask for fizzName
          *      ask for buzzName
          * 
-         * do phase 4
+         * do phase 5 (ALGORITHM)
          *      Algorithm.startAlgorithm(data);
          */
-        System.out.println("Enter 'exit' to quit the game");
-        /* if exit
-         * 
-         * do phase 0
-         *    System.exit(0);
-         * 
-         */
 
-        // Phase 2
+////////// Phase 3 (DEFAULT)
 
         // TODO: start default
 
-        // Phase 3
+////////// Phase 4 (SETUP)
 
         data.setLimit(input.getSetupInputInt("Enter the amount of numbers: ", "Limit set to: "));
         
@@ -65,7 +89,7 @@ public class Game {
 
         data.setBuzzName(input.getSetupInputString("Enter the second word: ", "Second word set to: "));
 
-        // Phase 4
+////////// Phase 5 (ALGORITHM)
 
         System.out.println("=================== INPUTS REGISTERED ===================");
         System.out.println("---------------------------------------------------------");
